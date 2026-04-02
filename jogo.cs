4456100e2bf8251bc06820e573855e40cb5073e8
jpcruz80121 <al80121@alunos.utad.pt>
2026-04-02 11:01:58 +0100

    Add files via upload

---
+++ jogo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UNO.UNO;
+
+public class Jogo
+{
+    public List<Jogador> Jogadores { get; set; } = new List<Jogador>();
+    public Baralho Baralho { get; set; } = new Baralho();
+    public List<Carta> PilhaDescarte { get; set; } = new List<Carta>();
+
+    public int JogadorAtualIndex { get; set; }
+    public bool SentidoHorario { get; set; } = true;
+
+    public Jogador JogadorAtual => Jogadores[JogadorAtualIndex];
+
+    public void ProximoJogador()
+    {
+        if (SentidoHorario)
+            JogadorAtualIndex = (JogadorAtualIndex + 1) % Jogadores.Count;
+        else
+            JogadorAtualIndex = (JogadorAtualIndex - 1 + Jogadores.Count) % Jogadores.Count;
+    }
+}
