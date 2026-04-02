d961f733120ffe7b3b7d46894f0b559c5d95a29b
jpcruz80121 <al80121@alunos.utad.pt>
2026-04-02 11:54:10 +0100

    baralho

---
+++ baralho.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Baralho
+{
+    public List<Carta> Cartas { get; set; } = new List<Carta>();
+
+    public void Embaralhar()
+    {
+        Random rnd = new Random();
+        int n = Cartas.Count;
+
+        while (n > 1)
+        {
+            n--;
+            int k = rnd.Next(n + 1);
+            var value = Cartas[k];
+            Cartas[k] = Cartas[n];
+            Cartas[n] = value;
+        }
+    }
+
+    public Carta ComprarCarta()
+    {
+        if (Cartas.Count == 0) return null;
+
+        Carta carta = Cartas[0];
+        Cartas.RemoveAt(0);
+        return carta;
+    }
+}
